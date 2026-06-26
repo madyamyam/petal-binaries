2a0f18cbf7766dd4c495db6cf953a7f7c0f4b4b0
yamlrider <madyamyam@tutamail.com>
2026-06-25 23:01:36 -0700

    added install flags to simplify the copy process

--- build-linux-x86_64.sh
+++ build-linux-x86_64.sh
@@ -1,50 +1,64 @@
 #!/bin/bash
 
-apt update && apt install -y nasm
+apt update && apt install -y nasm patchelf libjson-perl
 
+linux_install_root='install/linux-x86_64'
+linux_install_lib="$linux_install_root/lib"
 linux_bin_root='bin/linux-x86_64'
 mkdir -p $linux_bin_root
 
 sdl_flags_common='-DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RELEASE'
-sdl_flags='-DSDL_INSTALL=ON -DSDL_TEST_LIBRARY=OFF -DSDL_TESTS=OFF -DSDL_INSTALL_TESTS=OFF -DSDL_TESTS_LINK_SHARED=OFF -DSDL_EXAMPLES=OFF -DSDL_EXAMPLES_LINK_SHARED=OFF'
+sdl_flags='-DSDL_TEST_LIBRARY=OFF -DSDL_TESTS=OFF -DSDL_INSTALL_TESTS=OFF -DSDL_TESTS_LINK_SHARED=OFF -DSDL_EXAMPLES=OFF -DSDL_EXAMPLES_LINK_SHARED=OFF -DSDL_INSTALL=ON'
 # only support jpg, png, svg(?), webp
-sdl_image_flags='-DSDLIMAGE_STRICT=ON -DSDLIMAGE_INSTALL=OFF -DSDLIMAGE_INSTALL_CPACK=OFF -DSDLIMAGE_INSTALL_MAN=OFF -DSDLIMAGE_DEPS_SHARED=OFF -DSDLIMAGE_VENDORED=ON -DSDLIMAGE_SAMPLES_INSTALL=OFF -DSDLIMAGE_ANI=OFF -DSDLIMAGE_AVIF=OFF -DSDLIMAGE_BMP=OFF -DSDLIMAGE_GIF=OFF -DSDLIMAGE_JXL=OFF -DSDLIMAGE_LBM=OFF -DSDLIMAGE_PCX=OFF -DSDLIMAGE_PNM=OFF -DSDLIMAGE_QOI=OFF -DSDLIMAGE_TGA=OFF -DSDLIMAGE_TIF=OFF -DSDLIMAGE_XCF=OFF -DSDLIMAGE_XPM=OFF -DSDLIMAGE_XV=OFF'
-sdl_mixer_flags='-DSDLMIXER_STRICT=ON -DSDLMIXER_INSTALL=OFF -DSDLMIXER_INSTALL_CPACK=OFF -DSDLMIXER_INSTALL_MAN=OFF -DSDLMIXER_DEPS_SHARED=OFF -DSDLMIXER_VENDORED=ON -DSDLMIXER_SAMPLES_INSTALL=OFF -DSDLMIXER_FLAC=ON -DSDLMIXER_MOD=OFF -DSDLMIXER_MP3=ON -DSDLMIXER_MIDI=ON -DSDLMIXER_OPUS=ON -DSDLMIXER_WAVPACK=OFF'
-sdl_ttf_flags='-DSDLTTF_STRICT=ON -DSDLTTF_INSTALL=OFF -DSDLTTF_INSTALL_CPACK=OFF -DSDLTTF_INSTALL_MAN=OFF -DSDLTTF_VENDORED=ON -DSDLTTF_SAMPLES_INSTALL=OFF'
+sdl_image_flags='-DSDLIMAGE_STRICT=ON -DSDLIMAGE_INSTALL_CPACK=OFF -DSDLIMAGE_INSTALL_MAN=OFF -DSDLIMAGE_DEPS_SHARED=OFF -DSDLIMAGE_VENDORED=ON -DSDLIMAGE_SAMPLES_INSTALL=OFF -DSDLIMAGE_ANI=OFF -DSDLIMAGE_AVIF=OFF -DSDLIMAGE_BMP=OFF -DSDLIMAGE_GIF=OFF -DSDLIMAGE_JXL=OFF -DSDLIMAGE_LBM=OFF -DSDLIMAGE_PCX=OFF -DSDLIMAGE_PNM=OFF -DSDLIMAGE_QOI=OFF -DSDLIMAGE_TGA=OFF -DSDLIMAGE_TIF=OFF -DSDLIMAGE_XCF=OFF -DSDLIMAGE_XPM=OFF -DSDLIMAGE_XV=OFF -DSDLIMAGE_INSTALL=ON'
+sdl_mixer_flags='-DSDLMIXER_STRICT=ON -DSDLMIXER_INSTALL_CPACK=OFF -DSDLMIXER_INSTALL_MAN=OFF -DSDLMIXER_DEPS_SHARED=OFF -DSDLMIXER_VENDORED=ON -DSDLMIXER_SAMPLES_INSTALL=OFF -DSDLMIXER_FLAC=ON -DSDLMIXER_MOD=OFF -DSDLMIXER_MP3=ON -DSDLMIXER_MIDI=ON -DSDLMIXER_OPUS=ON -DSDLMIXER_WAVPACK=OFF -DSDLMIXER_INSTALL=ON'
+sdl_ttf_flags='-DSDLTTF_STRICT=OFF -DSDLTTF_INSTALL_CPACK=OFF -DSDLTTF_INSTALL_MAN=OFF -DSDLTTF_VENDORED=OFF -DSDLTTF_SAMPLES_INSTALL=OFF -DSDLTTF_INSTALL=ON'
 # BUILD_SHARED_LIBS is set to OFF when using vendored dependencies with DXC enabled
-sdl_shadercross_flags='-DSDLSHADERCROSS_DXC=ON -DSDLSHADERCROSS_SHARED=ON -DSDLSHADERCROSS_STATIC=OFF -DSDLSHADERCROSS_SPIRVCROSS_SHARED=OFF -DSDLSHADERCROSS_VENDORED=ON -DSDLSHADERCROSS_CLI=OFF -DSDLSHADERCROSS_INSTALL=OFF -DSDLSHADERCROSS_INSTALL_CPACK=OFF -DSDLSHADERCROSS_INSTALL_MAN=OFF'
+sdl_shadercross_flags='-DSDLSHADERCROSS_DXC=ON -DSDLSHADERCROSS_SHARED=ON -DSDLSHADERCROSS_STATIC=OFF -DSDLSHADERCROSS_SPIRVCROSS_SHARED=OFF -DSDLSHADERCROSS_VENDORED=ON -DSDLSHADERCROSS_CLI=OFF -DSDLSHADERCROSS_INSTALL_CPACK=OFF -DSDLSHADERCROSS_INSTALL_MAN=OFF -DSDLSHADERCROSS_INSTALL=ON -DSDLSHADERCROSS_INSTALL_RUNTIME=ON'
 
 # SDL
 cmake -S SDL -B sdl-build-release $sdl_flags_common $sdl_flags
-cmake --build sdl-build-release --target install --config Release
-strip -S sdl-build-release/libSDL3.so
-cp sdl-build-release/libSDL3.so $linux_bin_root/
+cmake --build sdl-build-release --config Release
+cmake --install sdl-build-release --prefix $linux_install_root
 
-# SDL_shadercross
-cmake -S SDL_shadercross SDL_shadercross-build-release $sdl_flags_common $sdl_shadercross_flags
-cmake --build SDL_shadercross-build-release --config Release
-strip -S SDL_shadercross-build-release/libSDL3_shadercross.so
-cp SDL_shadercross-build-release/libSDL3_shadercross.so $linux_bin_root/
-strip -S SDL_shadercross-build-release/external/DirectXShaderCompiler/lib/libdxcompiler.so
-cp SDL_shadercross-build-release/external/DirectXShaderCompiler/lib/libdxcompiler.so $linux_bin_root/
-# probably not strictly necessary
-strip -S SDL_shadercross-build-release/external/DirectXShaderCompiler/lib/libdxil.so
-cp SDL_shadercross-build-release/external/DirectXShaderCompiler/lib/libdxil.so $linux_bin_root/
+strip -S "$linux_install_root"/libSDL3.so
+cp "$linux_install_root"/libSDL3.so $linux_bin_root/
 
 # SDL_image
 cmake -S SDL_image sdl_image-build-release $sdl_flags_common $sdl_image_flags
 cmake --build sdl_image-build-release --config Release
-strip -S sdl_image-build-release/libSDL3_image.so
-cp sdl_image-build-release/libSDL3_image.so $linux_bin_root/
+cmake --install sdl_image-build-release --prefix $linux_install_root
+
+strip "$linux_install_root"/libSDL3_image.so
+cp "$linux_install_root"/libSDL3_image.so $linux_bin_root/
 
 # SDL_mixer
 cmake -S SDL_mixer sdl_mixer-build-release $sdl_flags_common $sdl_mixer_flags
 cmake --build sdl_mixer-build-release --config Release
-strip -S sdl_mixer-build-release/libSDL3_mixer.so
-cp sdl_mixer-build-release/libSDL3_mixer.so $linux_bin_root/
+cmake --install sdl_mixer-build-release --prefix $linux_install_root
+
+strip "$linux_install_root"/libSDL3_mixer.so
+cp "$linux_install_root"/libSDL3_mixer.so $linux_bin_root/
 
 # SDL_ttf
-cmake -S SDL_ttf SDL_ttf-build-release $sdl_flags_common $sdl_ttf_flags
-cmake --build SDL_ttf-build-release --config Release
-strip -S SDL_ttf-build-release/libSDL3_ttf.so
-cp SDL_ttf-build-release/libSDL3_ttf.so $linux_bin_root/
+cmake -S SDL_ttf sdl_ttf-build-release $sdl_flags_common $sdl_ttf_flags
+cmake --build sdl_ttf-build-release --config Release
+cmake --install sdl_ttf-build-release --prefix $linux_install_root
+
+strip "$linux_install_root"/libSDL3_ttf.so
+cp "$linux_install_root"/libSDL3_ttf.so $linux_bin_root/
+
+# SDL_shadercross
+# we won't be using DXBC, don't include libvkd3d-utils
+cmake SDL_shadercross sdl_shadercross-build-release $sdl_flags_common $sdl_shadercross_flags
+cmake --build sdl_shadercross-build-release --config Release
+cmake --install sdl_shadercross-build-release --prefix $linux_install_root
+
+strip "$linux_install_root"/libSDL3_shadercross.so
+cp "$linux_install_root"/libSDL3_shadercross.so $linux_bin_root/
+
+strip "$linux_install_root"/libdxcompiler.so
+cp "$linux_install_root"/libdxcompiler $linux_bin_root/
+
+strip "$linux_install_root"/libdxil.so
+cp "$linux_install_root"/libdxil.so $linux_bin_root/
--- build-windows-x86_64.ps1
+++ build-windows-x86_64.ps1
@@ -1,18 +1,20 @@
 # consider just downloading from releases
 
-choco install nasm webp
+choco install nasm webp # webp lib needed?
 
+$macos_install_root='install/windows-x86_64'
+$macos_install_lib="$windows_install_root/Release/lib" # dear msvc i beseech you let this be the right directory
 $windows_bin_root='bin/windows-x86_64'
 mkdir -p $windows_bin_root
 
 $sdl_flags_common=@('-DBUILD_SHARED_LIBS=ON','-DCMAKE_BUILD_TYPE=RELEASE')
-$sdl_flags=('-DSDL_INSTALL=ON','-DSDL_TEST_LIBRARY=OFF','-DSDL_TESTS=OFF','-DSDL_INSTALL_TESTS=OFF','-DSDL_TESTS_LINK_SHARED=OFF','-DSDL_EXAMPLES=OFF','-DSDL_EXAMPLES_LINK_SHARED=OFF') 
+$sdl_flags=('-DSDL_TEST_LIBRARY=OFF','-DSDL_TESTS=OFF','-DSDL_INSTALL_TESTS=OFF','-DSDL_TESTS_LINK_SHARED=OFF','-DSDL_EXAMPLES=OFF','-DSDL_EXAMPLES_LINK_SHARED=OFF','-DSDL_INSTALL=ON')
 # only support jpg, png, svg(?), webp
-$sdl_image_flags=@('-DSDLIMAGE_STRICT=ON','-DSDLIMAGE_INSTALL=OFF','-DSDLIMAGE_INSTALL_CPACK=OFF','-DSDLIMAGE_INSTALL_MAN=OFF','-DSDLIMAGE_DEPS_SHARED=OFF','-DSDLIMAGE_VENDORED=ON','-DSDLIMAGE_SAMPLES=OFF','-DSDLIMAGE_ANI=OFF','-DSDLIMAGE_AVIF=OFF','-DSDLIMAGE_BMP=OFF','-DSDLIMAGE_GIF=OFF','-DSDLIMAGE_JXL=OFF','-DSDLIMAGE_LBM=OFF','-DSDLIMAGE_PCX=OFF','-DSDLIMAGE_PNM=OFF','-DSDLIMAGE_QOI=OFF','-DSDLIMAGE_TGA=OFF','-DSDLIMAGE_TIF=OFF','-DSDLIMAGE_XCF=OFF','-DSDLIMAGE_XPM=OFF','-DSDLIMAGE_XV=OFF')
-$sdl_mixer_flags=@('-DSDLMIXER_STRICT=ON','-DSDLMIXER_INSTALL=OFF','-DSDLMIXER_INSTALL_CPACK=OFF','-DSDLMIXER_INSTALL_MAN=OFF','-DSDLMIXER_DEPS_SHARED=OFF','-DSDLMIXER_VENDORED=ON','-DSDLMIXER_SAMPLES=OFF','-DSDLMIXER_FLAC=ON','-DSDLMIXER_MOD=OFF','-DSDLMIXER_MP3=ON','-DSDLMIXER_MIDI=ON','-DSDLMIXER_OPUS=ON','-DSDLMIXER_WAVPACK=OFF')
-$sdl_ttf_flags=@('-DSDLTTF_STRICT=ON','-DSDLTTF_INSTALL=OFF','-DSDLTTF_INSTALL_CPACK=OFF','-DSDLTTF_INSTALL_MAN=OFF','-DSDLTTF_VENDORED=ON','-DSDLTTF_SAMPLES=OFF')
+$sdl_image_flags=@('-DSDLIMAGE_STRICT=ON','-DSDLIMAGE_INSTALL_CPACK=OFF','-DSDLIMAGE_INSTALL_MAN=OFF','-DSDLIMAGE_DEPS_SHARED=OFF','-DSDLIMAGE_VENDORED=ON','-DSDLIMAGE_SAMPLES=OFF','-DSDLIMAGE_ANI=OFF','-DSDLIMAGE_AVIF=OFF','-DSDLIMAGE_BMP=OFF','-DSDLIMAGE_GIF=OFF','-DSDLIMAGE_JXL=OFF','-DSDLIMAGE_LBM=OFF','-DSDLIMAGE_PCX=OFF','-DSDLIMAGE_PNM=OFF','-DSDLIMAGE_QOI=OFF','-DSDLIMAGE_TGA=OFF','-DSDLIMAGE_TIF=OFF','-DSDLIMAGE_XCF=OFF','-DSDLIMAGE_XPM=OFF','-DSDLIMAGE_XV=OFF','-DSDLIMAGE_INSTALL=ON')
+$sdl_mixer_flags=@('-DSDLMIXER_STRICT=ON','-DSDLMIXER_INSTALL_CPACK=OFF','-DSDLMIXER_INSTALL_MAN=OFF','-DSDLMIXER_DEPS_SHARED=OFF','-DSDLMIXER_VENDORED=ON','-DSDLMIXER_SAMPLES=OFF','-DSDLMIXER_FLAC=ON','-DSDLMIXER_MOD=OFF','-DSDLMIXER_MP3=ON','-DSDLMIXER_MIDI=ON','-DSDLMIXER_OPUS=ON','-DSDLMIXER_WAVPACK=OFF','-DSDLMIXER_INSTALL=OFF')
+$sdl_ttf_flags=@('-DSDLTTF_STRICT=ON','-DSDLTTF_INSTALL_CPACK=OFF','-DSDLTTF_INSTALL_MAN=OFF','-DSDLTTF_VENDORED=ON','-DSDLTTF_SAMPLES=OFF','-DSDLTTF_INSTALL=OFF')
 # BUILD_SHARED_LIBS is set to OFF when using vendored dependencies with DXC enabled
-$sdl_shadercross_flags=@('-DSDLSHADERCROSS_DXC=ON','-DSDLSHADERCROSS_SHARED=ON','-DSDLSHADERCROSS_STATIC=OFF','-DSDLSHADERCROSS_SPIRVCROSS_SHARED=OFF','-DSDLSHADERCROSS_VENDORED=ON','-DSDLSHADERCROSS_CLI=OFF','-DSDLSHADERCROSS_INSTALL=OFF','-DSDLSHADERCROSS_INSTALL_CPACK=OFF','-DSDLSHADERCROSS_INSTALL_MAN=OFF')
+$sdl_shadercross_flags=@('-DSDLSHADERCROSS_DXC=ON','-DSDLSHADERCROSS_SHARED=ON','-DSDLSHADERCROSS_STATIC=OFF','-DSDLSHADERCROSS_SPIRVCROSS_SHARED=OFF','-DSDLSHADERCROSS_VENDORED=ON','-DSDLSHADERCROSS_CLI=OFF','-DSDLSHADERCROSS_INSTALL_CPACK=OFF','-DSDLSHADERCROSS_INSTALL_MAN=OFF','-DSDLSHADERCROSS_INSTALL=ON','-DSDLSHADERCROSS_INSTALL_RUNTIME=ON')
 
 $sdl_flags = $($sdl_flags; $sdl_flags_common)
 $sdl_image_flags = $($sdl_image_flags; $sdl_flags_common)
@@ -22,35 +24,47 @@ $sdl_shadercross_flags = $($sdl_shadercross_flags; $sdl_flags_common)
 
 # SDL
 cmake -S SDL -B sdl-build-release $sdl_flags[0..$sdl_flags.Count]
-cmake --build sdl-build-release --target install --config Release
-strip -S sdl-build-release/Release/SDL3.dll
-cp sdl-build-release/Release/SDL3.dll $windows_bin_root/
+cmake --build sdl-build-release  --target install --config Release
+cmake --install sdl-build-release --prefix $windows_install_root
 
-# SDL_shadercross
-cmake -S SDL_shadercross SDL_shadercross-build-release $sdl_shadercross_flags[0..$sdl_shadercross_flags.Count]
-cmake --build SDL_shadercross-build-release --config Release
-strip -S SDL_shadercross-build-release/Release/SDL3_shadercross.dll
-cp SDL_shadercross-build-release/Release/SDL3_shadercross.dll $windows_bin_root/
-strip -S SDL_shadercross-build-release/external/DirectXShaderCompiler/Release/bin/dxcompiler.dll
-cp SDL_shadercross-build-release/external/DirectXShaderCompiler/Release/bin/dxcompiler.dll $windows_bin_root/
-# probably not strictly necessary
-strip -S SDL_shadercross-build-release/external/DirectXShaderCompiler/Release/bin/dxil.dll
-cp SDL_shadercross-build-release/external/DirectXShaderCompiler/Release/bin/dxil.dll $windows_bin_root/
+strip -S "$windows_install_root"/libSDL3.dll
+cp "$windows_install_root"/SDL3.dll $windows_bin_root/
 
 # SDL_image
 cmake -S SDL_image sdl_image-build-release $sdl_image_flags[0..$sdl_image_flags.Count]
 cmake --build sdl_image-build-release --config Release
-strip -S sdl_image-build-release/Release/SDL3_image.dll
-cp sdl_image-build-release/Release/SDL3_image.dll $windows_bin_root/
+cmake --install sdl_image-build-release --prefix $windows_install_root
+
+strip "$windows_install_root"/SDL3_image.dll
+cp "$windows_install_root"/SDL3_image.dll $windows_bin_root/
 
 # SDL_mixer
 cmake -S SDL_mixer sdl_mixer-build-release $sdl_mixer_flags[0..$sdl_mixer_flags.Count]
 cmake --build sdl_mixer-build-release --config Release
-strip -S sdl_mixer-build-release/Release/SDL3_mixer.dll
-cp sdl_mixer-build-release/Release/SDL3_mixer.dll $windows_bin_root/
+cmake --install sdl_mixer-build-release --prefix $windows_install_root
+
+strip "$windows_install_root"/SDL3_mixer.dll
+cp "$windows_install_root"/SDL3_mixer.dll $windows_bin_root/
 
 # SDL_ttf
 cmake -S SDL_ttf sdl_ttf-build-release $sdl_ttf_flags[0..$sdl_ttf_flags.Count]
 cmake --build sdl_ttf-build-release --config Release
-strip -S sdl_ttf-build-release/Release/SDL3_ttf.dll
-cp sdl_ttf-build-release/Release/SDL3_ttf.dll $windows_bin_root/
+cmake --install sdl_ttf-build-release --prefix $windows_install_root
+
+strip "$windows_install_root"/SDL3_ttf.dll
+cp "$windows_install_root"/SDL3_ttf.dll $windows_bin_root/
+
+# SDL_shadercross
+# we won't be using DXBC
+cmake SDL_shadercross sdl_shadercross-build-release $sdl_shadercross_flags[0..$sdl_shadercross_flags.Count]
+cmake --build sdl_shadercross-build-release --config Release
+cmake --install sdl_shadercross-build-release --prefix $windows_install_root
+
+strip "$windows_install_root"/SDL3_shadercross.dll
+cp "$windows_install_root"/SDL3_shadercross.dll $windows_bin_root/
+
+strip "$windows_install_root"/dxcompiler.dll
+cp "$windows_install_root"/dxcompiler $windows_bin_root/
+
+strip "$windows_install_root"/dxil.dll
+cp "$windows_install_root"/dxil.dll $windows_bin_root/
